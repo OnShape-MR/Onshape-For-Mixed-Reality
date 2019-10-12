97508f1fb0390698963ff1bf4e0cfa9bb3a718ca
Thib Grad <thibault.grad@gmail.com>
2019-10-12 15:58:12 +0200

    Added Documents Menu

---
+++ Assets/OnShape/MenuDocument.cs
@@ -0,0 +1,48 @@
+﻿using IO.Swagger.Model;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+public class MenuDocument : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject DocumentPrefab;
+    public GameObject ContentContainer;
+    private GameObject _currentgo;
+    private TextMeshProUGUI _textcomp;
+    private Image _imagethumbnail;
+    public Sprite Imagetoshow;
+    private Button _currentbutton;
+   
+
+
+    void Start()
+    {
+        var _json = Resources.Load<TextAsset>("documents");
+        RefreshDocumentList (JsonConvert.DeserializeObject<DocumentsGetDocumentsResponse200>(_json.text));
+    }
+    public void  RefreshDocumentList(DocumentsGetDocumentsResponse200 List)
+    {
+        foreach (DocumentsGetDocumentsResponse200Items _currentitem  in List.Items)
+        {
+            _currentgo= Instantiate(DocumentPrefab,ContentContainer.transform);
+            _textcomp = _currentgo.GetComponentInChildren<TextMeshProUGUI>();
+            _textcomp.text = "Document Name : " + _currentitem.Name + "\nLast modified by : " + _currentitem.ModifiedBy.Name + " on " + _currentitem.ModifiedAt + "\nOwned by : " + _currentitem.Owner.Name;
+            _currentbutton = _currentgo.GetComponentInChildren<Button>();
+            _imagethumbnail = _currentgo.GetComponentInChildren<Image>();
+            _imagethumbnail.sprite = Imagetoshow;
+            _currentbutton.onClick.AddListener(() =>
+            {
+                Debug.Log(_currentitem.Id);
+            });
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
