bce54c3c3736f6d0ff88897b1e623d4c329863b8
Thib Grad <thibault.grad@gmail.com>
2019-10-15 22:02:16 +0200

    Update MRTK, add show mapping

---
+++ Assets/OnShape/SpatiaMappingEnabling.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatiaMappingEnabling : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ShowMapping()
+    {
+        // Cast the Spatial Awareness system to IMixedRealityDataProviderAccess to get an Observer
+        var access = CoreServices.SpatialAwarenessSystem as IMixedRealityDataProviderAccess;
+
+        // Get the first Mesh Observer available, generally we have only one registered
+        var observer = access.GetDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
+
+        // Resumes observation of spatial mesh data
+        observer.DisplayOption=SpatialAwarenessMeshDisplayOptions.Visible;
+
+    }
+    public void HideMapping()
+    {
+        // Cast the Spatial Awareness system to IMixedRealityDataProviderAccess to get an Observer
+        var access = CoreServices.SpatialAwarenessSystem as IMixedRealityDataProviderAccess;
+
+        // Get the first Mesh Observer available, generally we have only one registered
+        var observer = access.GetDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
+
+        // Resumes observation of spatial mesh data
+        observer.DisplayOption = SpatialAwarenessMeshDisplayOptions.None;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
